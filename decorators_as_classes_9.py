505e35c30fb03868af0e54fff2fbda52000cd46b
Scott Volk <scottvolk@gmail.com>
2019-08-31 17:39:30 -0700

    Removing the 9_1 review.

--- decorators_as_classes_9.py
+++ decorators_as_classes_9.py
@@ -1,4 +1,5 @@
-# if you want decorators to work both inside and outside of classe definitions
+# if you want decorators to work both inside and outside of class definitions
+
 import types
 from functools import wraps
 
@@ -8,13 +9,18 @@ class Profiled:
     self.ncalls = 0
 
   def __call__(self, *args, **kwargs):
+    '''making a callable class'''
     self.ncalls += 1
+    # __wrapped__ contains the original func
     return self.__wrapped__(*args, **kwargs)
 
   def __get__(self, instance, cls):
+    '''still don't understand how this works'''
     if instance is None:
+      # this is never run
       return self
     else:
+      print("__get__ ", self, instance)
       return types.MethodType(self, instance)
 
 @Profiled
@@ -26,12 +32,21 @@ class Spam:
   def bar(self, x):
     print(self, x)
 
+  @classmethod
+  @Profiled
+  def foo(cls, x):
+    print(cls, x)
+
 print(add(2, 3))
 print(add(4, 5))
 print(add.ncalls)
 
 s = Spam()
-print(s.bar(1))
-print(Spam.bar.ncalls)
+s.bar(1)
+s.bar(2)
+print(s.bar.ncalls)
 
-
+Spam.foo(2)
+Spam.foo(3)
+Spam.foo(4)
+print(Spam.foo.ncalls)
